2ea2c15bc921527fbfc44f9fd153bb2de37bb1db
Marc Falzon <marc@exoscale.com>
2020-11-07 19:13:43 +0100

    Update comment

--- error.go
+++ error.go
@@ -5,5 +5,5 @@ import "errors"
 // ErrCancelled represents an error reported if a plan is cancelled.
 var ErrCancelled = errors.New("plan execution cancelled")
 
-// ErrTimeout represents an error reported if a plan takes too long to execute.
+// ErrTimeout represents an error reported if a plan took too long to execute.
 var ErrTimeout = errors.New("plan execution duration limit exceeded")
