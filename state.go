5dd404959d1cdd4a4a6f62423836857a10017499
Marc Falzon <marc@exoscale.com>
2020-11-07 11:14:21 +0100

    fixup! WIP

--- state.go
+++ state.go
@@ -2,8 +2,19 @@ package gsd
 
 import "sync"
 
-// State represents a key/value map provided to the plan's steps for sharing state between steps.
-// It is a wrapper around a sync.Map, so it is safe to be used concurrently.
+// State represents a key/value map provided to the plan's steps for sharing
+// state between steps. It is a wrapper around a sync.Map, so it is safe to be
+// used concurrently.
 type State struct {
 	sync.Map
+}
+
+// Get returns the stored value corresponding to the key k, or the nil value
+// if the requested key is not found in the state.
+func (s *State) Get(k string) interface{} {
+	if v, ok := s.Load(k); ok {
+		return v
+	}
+
+	return nil
 }
